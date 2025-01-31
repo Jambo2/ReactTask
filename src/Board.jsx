53d0bd94191a8dbae54c5e3e2de7c7920c0d384e
Jambo2 <egor.zaytsev.2018@list.ru>
2025-01-31 11:12:40 +0300

    fix

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,44 +1,61 @@
-import { useState } from 'react'
-import { useBoardsStore } from './store/store'
-import './app/styles/Board.css'
+import { useState } from 'react';
+import { useBoardsStore } from './store/store';
+import './app/styles/Board.css';
 
-export default function Board(){
-    // const nameBoards = useBoardsStore(state=>state.boardsList)
+export default function Board() {
+    const nameBoards = useBoardsStore(state => state.boardsList);
+    const addNewBoard = useBoardsStore(state => state.addNewBoard); 
 
-    const [Board, SetBoard] = useState(false)
+    const [isBoardVisible, setIsBoardVisible] = useState(false);
+    const [newBoardName, setNewBoardName] = useState(''); 
 
-    function button__add(){
-        SetBoard(!Board)
+    function toggleAddBoard() {
+        setIsBoardVisible(!isBoardVisible);
+        setNewBoardName(''); 
     }
-  
-    // const boardsList = nameBoards?.map((item, index) => {return <li key={index}>{item}</li>}
-    // );   
 
-    return(
+    const handleSave = (event) => {
+        event.preventDefault(); 
+        if (newBoardName.trim()) { 
+            addNewBoard(newBoardName); 
+            setNewBoardName(''); 
+            setIsBoardVisible(false); 
+        }
+    };
+
+    const boardsList = nameBoards.map((item, index) => (
+        <li key={index}>{item}</li>
+    ));
+
+    return (
         <>
-        <div className='block__add'>
-            <button onClick={button__add} className="addBoard">
-                <box-icon className='button__icon' name='add-to-queue' color='white'></box-icon>
-                Новая доска
-            </button>
-            {
-                Board && 
-                <div className='create__board'> 
-                    <form onSubmit={event=>event.preventDefault()}>
-                        <h3>Название доски</h3>
-                        <input type="text" pceholder='Моя доска'/>
-                        <div className='button__form'>la
-                            <button  type='button' onClick={()=>SetBoard(false)}>Отмена</button>
-                            <button>Сохранить</button>
-                        </div>
-                    </form>
-                </div>
-            }
-        </div>
-        {/* <div>
-           <ul>{boardsList}</ul>
-        </div> */}
+            <div className='block__add'>
+                <button onClick={toggleAddBoard} className="addBoard">
+                    <box-icon className='button__icon' name='add-to-queue' color='white'></box-icon>
+                    Новая доска
+                </button>
+                {
+                    isBoardVisible &&
+                    <div className='create__board'>
+                        <form onSubmit={handleSave}>
+                            <h3>Название доски</h3>
+                            <input 
+                                type="text" 
+                                placeholder='Моя доска' 
+                                value={newBoardName} 
+                                onChange={(event) => setNewBoardName(event.target.value)} 
+                            />
+                            <div className='button__form'>
+                                <button type='button' onClick={toggleAddBoard}>Отмена</button>
+                                <button type='submit'>Сохранить</button> 
+                            </div>
+                        </form>
+                    </div>
+                }
+            </div>
+            <div>
+                <ul>{boardsList}</ul>
+            </div>
         </>
-    )
+    );
 }
-
